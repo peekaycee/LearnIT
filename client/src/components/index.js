663ebd5531ec9cf55af81b16b163388678bfabdf
KALU KALU OKOMBA <54875766+peekaycee@users.noreply.github.com>
2024-02-07 13:09:25 +0100

    Delete client directory

--- client/src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Header } from './header/Header';
-export { default as Navbar } from './navbar/Navbar';
-export { default as About } from './about/About';
-export { default as Courses } from './courses/Courses';
-export { default as Contact } from './contact/Contact';
-export { default as Library } from './library/Library';
-export { default as Footer } from './footer/Footer';
