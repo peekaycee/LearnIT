adc16eb2709847f28fd769a73f156c015fab8a57
KALU KALU OKOMBA <kalukaluokomba@gmail.com>
2024-02-11 00:08:18 +0100

    updated password visibility

--- client/src/components/signIn/SignIn.jsx
+++ client/src/components/signIn/SignIn.jsx
@@ -1,75 +1,86 @@
 import './signIn.css';
 import { Link } from 'react-router-dom';
-import { openEye, google, github, facebook } from '../../assets'
+import { google, github, facebook, closeEye, openEye } from '../../assets';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
-import { IoIosClose } from "react-icons/io";
+import { IoIosClose } from 'react-icons/io';
 import { useState } from 'react';
 
 // eslint-disable-next-line react/prop-types
-const SignIn = ({ onClose }) => { 
+const SignIn = ({ onClose }) => {
   const [toggleSigninCard, setToggleSigninCard] = useState(true);
+  const [showPassword, setShowPassword] = useState(false);
 
   return (
     <div className='sign__in-container'>
-    {toggleSigninCard && (
-      <div className='signin__card'>
-        <form id='signin__form'>
-          <div className="closeBtn">
-            <IoIosClose className='close__btn' onClick={()=>{
-              setToggleSigninCard(false);
-              onClose();
-            }} />
+      {toggleSigninCard && (
+        <div className='signin__card'>
+          <form id='signin__form'>
+            <div className='closeBtn'>
+              <IoIosClose
+                className='close__btn'
+                onClick={() => {
+                  setToggleSigninCard(false);
+                  onClose();
+                }}
+              />
+            </div>
+            <h1>LOGIN</h1>
+            <div className='email'>
+              <input
+                type='email'
+                name='email'
+                id='email'
+                placeholder='Email'
+                required
+              />
+            </div>
+            <div className='password__card'>
+              <input
+                type={showPassword ? 'text' : 'password'}
+                name='password'
+                id='password'
+                placeholder='Password'
+                required
+              />
+              <LazyLoadImage
+                id='open__eye'
+                src={showPassword ? openEye : closeEye}
+                alt='close__eye'
+                onClick={() => {
+                  setShowPassword(!showPassword);
+                }}
+              />
+            </div>
+            <div className='submit__btn'>
+              <button type='submit'>SIGN IN</button>
+            </div>
+          </form>
+          <div className='sign__in--text'>
+            <Link to='#'>
+              <p>forgot password?</p>
+            </Link>
+            <p>or</p>
+            <p>sign in with</p>
           </div>
-          <h1>LOGIN</h1>
-          <div className='email'>
-            <input
-              type='email'
-              name='email'
-              id='email'
-              placeholder='Email'
-              required
-            />
+          <div className='social__icons'>
+            <div className='google'>
+              <Link to='#'>
+                <LazyLoadImage src={google} alt='google thumbnail' />
+              </Link>
+            </div>
+            <div className='github'>
+              <Link to='#'>
+                <LazyLoadImage src={github} alt='github thumbnail' />
+              </Link>
+            </div>
+            <div className='facebook'>
+              <Link to='#'>
+                <LazyLoadImage src={facebook} alt='facebook thumbnail' />
+              </Link>
+            </div>
           </div>
-          <div className='password__card'>
-            <input
-              type='password'
-              name='password'
-              id='password'
-              placeholder='Password'
-              required
-            />
-            <LazyLoadImage  src={openEye} alt='open__eye' />
-          </div>
-          <div className='submit__btn'>
-            <button type='submit'>SIGN IN</button>
-          </div>
-        </form>
-        <div className='sign__in--text'>
-          <Link to='#'>
-            <p>forgot password?</p>
-          </Link>
-          <p>or</p>
-          <p>sign in with</p>
         </div>
-        <div className='social__icons'>
-          <div className='google'>
-            <Link to='#'>
-              <LazyLoadImage  src={google} alt='google thumbnail' />
-            </Link>
-          </div>
-          <div className='github'>
-            <Link to='#'>
-              <LazyLoadImage  src={github} alt='github thumbnail' />
-            </Link>
-          </div>
-          <div className='facebook'>
-            <Link to='#'>
-              <LazyLoadImage  src={facebook} alt='facebook thumbnail' />
-            </Link>
-          </div>
-        </div>
-      </div>)
-    }
+      )}
     </div>
   );
 };
--- client/src/components/signUp/SignUp.jsx
+++ client/src/components/signUp/SignUp.jsx
@@ -1,6 +1,6 @@
 import './signUp.css';
 import { Link } from 'react-router-dom';
-import { openEye, google, github, facebook } from '../../assets';
+import { google, github, facebook, closeEye, openEye } from '../../assets';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { IoIosClose } from 'react-icons/io';
 import { useState } from 'react';
@@ -8,16 +8,21 @@ import { useState } from 'react';
 // eslint-disable-next-line react/prop-types
 const SignUp = ({ onClose }) => {
   const [toggleSignupCard, setToggleSignupCard] = useState(true);
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <div className='sign__up-container'>
       {toggleSignupCard && (
         <div className='signup__card '>
           <form id='signup__form'>
             <div className='closeBtn'>
-              <IoIosClose className='close__btn' onClick={()=>{
-                setToggleSignupCard(false);
-                onClose();
-              }} />
+              <IoIosClose
+                className='close__btn'
+                onClick={() => {
+                  setToggleSignupCard(false);
+                  onClose();
+                }}
+              />
             </div>
             <h1>REGISTER</h1>
             <div className='email'>
@@ -31,13 +36,20 @@ const SignUp = ({ onClose }) => {
             </div>
             <div className='password__card'>
               <input
-                type='password'
+                type={showPassword ? 'text' : 'password'}
                 name='password'
                 id='password'
                 placeholder='Password'
                 required
               />
-              <LazyLoadImage src={openEye} alt='open__eye' />
+              <LazyLoadImage
+                id='open__eye'
+                src={showPassword ? openEye : closeEye}
+                alt='close__eye'
+                onClick={() => {
+                  setShowPassword(!showPassword);
+                }}
+              />
             </div>
             <div className='submit__btn'>
               <button type='submit'>SIGN UP</button>
@@ -64,8 +76,8 @@ const SignUp = ({ onClose }) => {
               </Link>
             </div>
           </div>
-        </div>)
-      }
+        </div>
+      )}
     </div>
   );
 };
