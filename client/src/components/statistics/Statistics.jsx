ae5cca6ef299e77b68470ce961bb78140087579d
KALU KALU OKOMBA <kalukaluokomba@gmail.com>
2024-02-27 14:00:23 +0100

    server removed

--- client/src/components/statistics/Statistics.jsx
+++ client/src/components/statistics/Statistics.jsx
@@ -12,36 +12,36 @@ const Statistics = () => {
           <img src={blog02} alt='' />
         </div>
         <article className='stats__content-data'>
-        <div className="data__container">
-          <div className='content__data'>
-            <div className='data'>
-              <h3>1</h3>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              </p>
+          <div className="data__container">
+            <div className='content__data'>
+              <div className='data'>
+                <h3>1</h3>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                </p>
+              </div>
+              <div className='data'>
+                <h3>2+</h3>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                </p>
+              </div>
             </div>
-            <div className='data'>
-              <h3>2+</h3>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              </p>
+            <div className='content__data'>
+              <div className='data'>
+                <h3>3+</h3>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                </p>
+              </div>
+              <div className='data'>
+                <h3>4</h3>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                </p>
+              </div>
             </div>
           </div>
-          <div className='content__data'>
-            <div className='data'>
-              <h3>3+</h3>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              </p>
-            </div>
-            <div className='data'>
-              <h3>4</h3>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              </p>
-            </div>
-          </div>
-        </div>
         </article>
       </div>
     </section>
